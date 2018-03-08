923bb032c971b61db53daf3abcdcf011eff0a9a8
Ancy <lazuliroseancy@gmail.com>
2018-03-09 05:18:27 +0900

    Add form area of index.html,renderer.js

---
+++ renderer.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("comment-form").onsubmit = () => {
+        const commentInput = document.getElementById("comment-input");
+
+        if (commentInput.value === "") {
+            return false;
+        }
+        const newComment = document.createElement("li");
+        newComment.innerHTML = commentInput.value;
+        document.getElementById("comments").appendChild(newComment);
+
+        commentInput.value = "";
+        return false;
+    }
+})
